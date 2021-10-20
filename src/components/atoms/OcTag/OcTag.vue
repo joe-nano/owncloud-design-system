cba5dbd5467b382680e7edfa2bede4d80ba952a5
Steve Frenzel <stevefrenzel@gmx.de>
2021-10-20 15:46:42 +0200

    feat(OcTag): removed docs element

--- src/components/atoms/OcTag/OcTag.vue
+++ src/components/atoms/OcTag/OcTag.vue
@@ -97,37 +97,3 @@ export default {
   }
 }
 </style>
-
-<docs>
-Component to display various information.
-```js
-<oc-grid>
-  <oc-tag>
-    <oc-icon name="link" />
-    Shared via link
-  </oc-tag>
-</oc-grid>
-```
-## Different types of the tag component
-The tag component can be rendered as a different element if desired. You can specify such element via property `type`.
-
-```js
-<oc-grid gutter="small" flex>
-  <div>
-    <oc-tag>
-      <oc-icon name="group" />
-      Shared with other people
-    </oc-tag>
-  </div>
-  <div>
-    <oc-tag type="a">
-      <oc-icon name="link" />
-      Shared via link
-    </oc-tag>
-  </div>
-  <div>
-    <oc-tag type="button">Expires in 2 days</oc-tag>
-  </div>
-</oc-grid>
-```
-</docs>
